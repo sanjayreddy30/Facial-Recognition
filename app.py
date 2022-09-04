f7060388f0eaf398ecffdfc957dbccfa4ce3e822
sanjayreddy30 <112507238+sanjayreddy30@users.noreply.github.com>
2022-09-04 20:39:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
-
+import os
 
 app = Flask(__name__)
 
@@ -20,5 +20,6 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0',port=port)
+    app.run(debug=True)
